203563137cfd71f015e370b1424923fd883d6ade
Srimathi-sri102 <pprasannagovindhan@gmail.com>
2025-12-24 16:23:23 +0530

    Add files via upload

---
+++ bavu.java
@@ -0,0 +1,23 @@
+package srii;
+import java.util.Scanner;
+
+public class bavu 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int i;
+		int s=sc.nextInt();
+		for (i=1;i<=s;i++)
+	    {
+	    System.out.println(s);
+	    }
+	}
+}
+
+		
+
+	
+
+	
